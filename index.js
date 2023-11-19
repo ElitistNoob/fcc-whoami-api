c8799c0c03a740a0a92d7caeb25e84af322b2488
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-19 18:01:42 +0700

    installed and configured eslint and prettier

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const express = require('express');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
-app.use(cors({optionsSuccessStatus: 200}))
+app.use(cors({ optionsSuccessStatus: 200 }));
 
-const absolutePath =  __dirname + '/views/index.html'
+const absolutePath = `${__dirname}/views/index.html`;
 
 app.use(express.static('public'));
 
-app.get((req, res, next) => {
-    const ip = req.ip;
-    console.log(ip)
-    res.send(ip);
-    next();
-}, (req, res) => {
-    console.log(res)
-})
-
 app.get('/', (req, res) => {
-    res.sendFile(absolutePath);
-})
+  res.sendFile(absolutePath);
+});
 
 app.use('/api/whoami', (req, res) => {
-    res.send({
-        "ipaddress": req.headers["host"].slice(0, 12),
-        "language": req.headers["accept-language"],
-        "software": req.headers["user-agent"]
-    })
-})
+  res.send({
+    ipaddress: req.headers.host.slice(0, 12),
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent'],
+  });
+});
 
-const listener = app.listen(process.env.PORT, (req, res) => {
-    console.log({'port': listener.address().port})
-})
+const listener = app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line
+  console.log({ port: listener.address().port });
+});
