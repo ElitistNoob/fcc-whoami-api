4b116616f85835a1b86d6f77923418127711e9fd
Kevin Hernot <kevin@labs.haystack.asia>
2023-11-19 17:19:54 +0700

    returning ipaddress, language and software in api response

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config()
-console.log({'env': process.env})
 
 const app = express();
 app.use(cors({optionsSuccessStatus: 200}))
@@ -10,15 +9,27 @@ const absolutePath =  __dirname + '/views/index.html'
 
 app.use(express.static('public'));
 
+app.get((req, res, next) => {
+    const ip = req.ip;
+    console.log(ip)
+    res.send(ip);
+    next();
+}, (req, res) => {
+    console.log(res)
+})
+
 app.get('/', (req, res) => {
     res.sendFile(absolutePath);
 })
 
 app.use('/api/whoami', (req, res) => {
-    const ip = req.ip
-    console.log({ip})
+    res.send({
+        "ipaddress": req.headers["host"].slice(0, 12),
+        "language": req.headers["accept-language"],
+        "software": req.headers["user-agent"]
+    })
 })
 
 const listener = app.listen(process.env.PORT, (req, res) => {
-    console.log('Listening on port' + listener.listen().port)
+    console.log({'port': listener.address().port})
 })
